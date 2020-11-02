8ff79d8834e04971d4838458c23c89678d4e1cc1
Debadeep <debadeep.sen@bandhan.org>
2020-11-02 10:55:36 +0530

    updates

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const firstLetter = (s) = s[0];
-const lastLetter = (s) = s[s.length - 1];
+const firstLetter = (s) => s[0];
+const lastLetter = (s) => s[s.length - 1];
+const randomLetter = (s) => {
+    let randomIndex = Math.floor(Math.random(s.length));
+    return s[randomIndex];
+}
 
-module.exports = { firstLetter, lastLetter }
+module.exports = { firstLetter, lastLetter, randomLetter }
